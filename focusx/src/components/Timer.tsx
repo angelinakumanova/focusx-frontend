a9e1734554d704fe7f348745a674e7981631f649
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-05-07 00:02:58 +0300

    Update timer completion pop up layout

--- focusx/src/components/Timer.tsx
+++ focusx/src/components/Timer.tsx
@@ -153,26 +153,32 @@ export const FocusTimer: React.FC = () => {
             initial={{ opacity: 0 }}
             exit={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
-            className="absolute inset-0 bg-zinc-800/95 flex items-center justify-center z-50"
+            transition={{ duration: 0.4 }}
+            className="absolute inset-0 bg-black/80  flex items-center justify-center z-50"
           >
-            <div className="text-center">
-              <span className="flex flex-col justify-center items-center">
-                <IconConfetti />
-                <h2 className="text-4xl font-bold text-white mb-4">
-                  Session Complete!
-                </h2>
-              </span>
+            <div className="bg-neutral-900 rounded-2xl px-8 py-10 shadow-xl max-w-md w-full text-center border border-white/10">
+              <div className="flex flex-col items-center space-y-4">
+                <div className="text-5xl">
+                  <IconConfetti className="w-12 h-12 text-yellow-400 animate-bounce" />
+                </div>
 
-              <p className="text-lg font-medium text-white mb-6">
-                You've finished all sets. Great job!
-              </p>
-              <button
-                onClick={() => setIsCompleted(false)}
-                className="px-6 py-3 bg-white text-zinc-900 font-semibold rounded-lg hover:bg-gray-200 hover:cursor-pointer transition duration-300"
-              >
-                Close
-              </button>
+                <h2 className="text-3xl font-bold text-white">
+                  Session Complete 
+                </h2>
+
+                <p className="text-base text-zinc-300">
+                  You've finished all your focus sets. Take a break or start
+                  another session when you're ready.
+                </p>
+
+                <button
+                  onClick={() => setIsCompleted(false)}
+                  className="mt-6 inline-block px-6 py-3 bg-gradient-to-r from-zinc-500 to-zinc-600
+                   text-white font-medium rounded-xl hover:scale-110 hover:cursor-pointer transition-transform duration-200"
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </motion.div>
         )}
