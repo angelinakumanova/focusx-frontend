940c999c1dbb8cfe3772f002c9b05a811839ab83
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-08-24 23:11:05 +0300

    Update api urls

--- focusx/src/services/baseUrls.ts
+++ focusx/src/services/baseUrls.ts
@@ -1,8 +1,8 @@
 export const BASE_URLS = {
   user: "https://focusx-user-service.onrender.com/api",
-  // session: "https://session-service-focusx.up.railway.app/api/sessions",
-  // goal: "https://goal-service-focusx.up.railway.app/api/goals"
+  goal: "https://focusx-goal-service.onrender.com/api/goals",
+  session: "https://focusx-session-service.onrender.com/api/sessions"
   // user: "http://localhost:8080/api",
-  goal: "http://localhost:8081/api/goals",
-  session: "http://localhost:8082/api/sessions",
+  // goal: "http://localhost:8081/api/goals",
+  // session: "http://localhost:8082/api/sessions",
 };
