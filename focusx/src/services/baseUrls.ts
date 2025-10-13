cfcd8c1b3c989a2095a2e88e37a1b16570ef9eb2
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-10-13 22:11:19 +0300

    Update baseUrls.ts

--- focusx/src/services/baseUrls.ts
+++ focusx/src/services/baseUrls.ts
@@ -1,7 +1,7 @@
 export const BASE_URLS = {
-  user: "https://focusx-user-service.onrender.com/api",
-  goal: "https://focusx-goal-service.onrender.com/api/goals",
-  session: "https://focusx-session-service.onrender.com/api/sessions",
+  user: "https://user-service-focusx-d8o3.onrender.com/api",
+  goal: "https://goal-service-focusx.onrender.com/api/goals",
+  session: "https://session-service-focusx.onrender.com/api/sessions",
   // user: "http://localhost:8080/api",
   // goal: "http://localhost:8081/api/goals",
   // session: "http://localhost:8082/api/sessions",
